1c1e55829e70785ad80e848895faa9d5d61b2429
memoryleak47 <memoryleak47@protonmail.ch>
2025-03-19 13:35:08 +0100

    remove outdated {build,use}-image.sh

--- build-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This file can be removed, it's just a helper for me.
-
-sudo docker build -t slotted-image .
-sudo docker save slotted-image -o slotted-image.tar
--- use-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This file can be removed, it's just a helper for me.
-
-sudo docker load -i slotted-image.tar
-sudo docker run -it slotted-image
